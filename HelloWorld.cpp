59f0e64b541d08f5b745df5ff5148333b697ed81
SinhaAmrit <56241123+SinhaAmrit@users.noreply.github.com>
2020-02-13 19:40:51 +0530

    HelloWorld.cpp Added

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+/**
+* @author : Amrit Sinha
+* Date : February 13,2020
+* GitHub : SinhaAmrit
+* Facebook : /codiizone
+* Instagram : @codiizone
+* wesite : https://codiizone.blogspot.com/
+* C++ Program To Print Hello World
+*/
+#include <iostream>
+using namespace std;
+int main() {
+ cout<<"Hello World!!";
+    return 0;
+}
