e8ff7be6ac198bd049f6676a6deb07b269fbb74f
SinhaAmrit <56241123+SinhaAmrit@users.noreply.github.com>
2020-02-13 22:53:13 +0530

    Create AddNum.cpp

---
+++ AddNum.cpp
@@ -0,0 +1,31 @@
+/*
+* @author : Amrit Sinha
+* Date : February 13,2020
+* GitHub : SinhaAmrit
+* Facebook : /codiizone
+* Instagram : @codiizone
+* wesite : https://codiizone.blogspot.com/
+* C++ Program To Accept Two Numbers And Display Their Sum
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    //variable declaration
+    int num1, num2, result;
+    
+    //take user input
+    cout << "Enter first Number : ";
+    cin >> num1;
+
+    cout << "\nEnter second Number : ";
+    cin >> num2;
+
+    //to add two number
+    result = num1 + num2;
+
+    //display addition
+    cout << "\nAddition is : " << result;
+
+    return 0;
+}
